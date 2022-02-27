65c27025373d9eeca1154db61e713f80fc175bc9
Seongjin Kim <mansj98@naver.com>
2022-02-27 15:17:58 +0900

    test

---
+++ def.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+
+a = int(input("a : "))
+b = int(input("b : "))
+print("add : ",add(a,b))
+print("sub : ",sub(a,b))
+print("mul : ",mul(a,b))
+print("div : ",div(a,b))
